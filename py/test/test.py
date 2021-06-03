4dedfa2d5af1c64849f2d31c1fe70d204422fdd6
z4v0 <alikdelta@wp.pl>
2021-06-03 11:22:33 +0200

    a

--- py/test/test.py
+++ py/test/test.py
@@ -1,9 +1,11 @@
+import os
 
+clear = lambda: os.system('cls')
 user_input = 0
 
 def save_to_file():
-    file = open("Uczen-Opiekun.txt", "r")
-    file2 = open("wynik.txt", "w")
+    file = open("input.txt", "r")
+    file2 = open("output.txt", "w")
     line = file.readline()
     i = 1
     while (line != ""):
@@ -11,18 +13,33 @@ def save_to_file():
         line = file.readline()
         i+=1
 
+def zaw2pol():
+    clear()
+    txt = str(input("Speak Zawish now: "))
+    txt += 'A'
+    print(txt + '\n')
 
-while user_input != 3:
+def pol2zaw():
+    clear()
+    txt = str(input("Speak Polish now: "))
+    txt += 'B'
+    print(txt + '\n')
+
+
+while user_input != 4:
 
     if user_input == 1:
-        print("1")
-    if user_input == 2:
         save_to_file()
+    if user_input == 2:
+        zaw2pol()
+    if user_input == 3:
+        pol2zaw()
         
     print()
-    print("1. Elo frajero")
-    print("2. Siema eniu")
-    print("3. Koniec")
+    print("1. Save To File")
+    print("2. Zawish -> Polish")
+    print("3. Polish -> Zawish")
+    print("4. Koniec")
 
-    user_input = int(input("Daj: "))
+    user_input = int(input("Give your input: "))
     print()
