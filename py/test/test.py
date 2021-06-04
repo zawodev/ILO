ff0d4f2952c4af13c5db4f3a30473d7db305921d
z4v0 <alikdelta@wp.pl>
2021-06-05 00:50:11 +0200

    a

--- py/test/test.py
+++ py/test/test.py
@@ -1,15 +1,15 @@
 import os
 import json
 
-with open("data.json", 'r') as jsonfile:
+with open("data4.json", 'r') as jsonfile:
     paren = json.loads(jsonfile.read())
 
 clear = lambda: os.system('cls')
 user_input = 0
-n = 2 #dla n=3 calkiem spora tablica sie robi takze przemysl zmiane tutaj zeby dzialalo to inaczej i nieco szybciej
-key = 69
+n = 3 #dla n=3 calkiem spora tablica sie robi takze przemysl zmiane tutaj zeby dzialalo to inaczej i nieco szybciej
+key = 1640 #3969
 
-symbols = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 def save_to_file():
     file = open("input.txt", "r")
@@ -23,39 +23,36 @@ def save_to_file():
 
 def zaw2pol():
     clear()
-    txt_in = str(input("Speak Zawish now: "))
+    txt_in = str(input("=============================================\n          Type Text to Translate:\n=============================================\n"))
     parts = [(txt_in[i:i+n]) for i in range(0, len(txt_in), n)]
     #print(parts)
 
     for i in range(0, int(len(txt_in) / n)):
-        index = paren.index(parts[i])
-        parts[i] = paren[(index + key) % len(paren)]
+        try: 
+            index = paren.index(parts[i])
+            parts[i] = paren[(index + 74438) % len(paren)]
+        except:
+            parts[i] = parts[i]
+        
 
     txt_out = "".join(parts)
     print(txt_out)
 
 
-def pol2zaw():
+def generate_symbols(): #148876 => 74438 * 2
     clear()
-    txt_in = str(input("Speak Zawish now: "))
-    parts = [(txt_in[i:i+n]) for i in range(0, len(txt_in), n)]
-    #print(parts)
-
-    for i in range(0, int(len(txt_in) / n)):
-        index = paren.index(parts[i])
-        parts[i] = paren[(index - key) % len(paren)]
-
-    txt_out = "".join(parts)
-    print(txt_out)
-
-
-def generate_symbols():
-    file = open("output.txt", "w")
-    file.write("[")
+    file = open("data4.json", "w")
+    txt = '['
     for i in range(0, len(symbols)):
         for j in range(0, len(symbols)):
-            file.write('"' + symbols[i] + symbols[j] + '", ')
-    file.write("]")
+            for k in range(0, len(symbols)):
+                if symbols[i] != ' ' or symbols[j] != ' ' or symbols[k] != ' ': 
+                    txt += '"' + symbols[i] + symbols[j] + symbols[k] +'", '
+    txt = txt[:len(txt)-2] + ']'
+    file.write(txt)
+    print("=============================================\n       Symbols Generated Successfully!\n=============================================")
+    
+
 
 while user_input != 4:
 
@@ -64,12 +61,12 @@ while user_input != 4:
     if user_input == 2:
         zaw2pol()
     if user_input == 3:
-        pol2zaw()
+        generate_symbols()
         
     print()
     print("1. Save To File")
-    print("2. Zawish -> Polish")
-    print("3. Polish -> Zawish")
+    print("2. Zawish <=> Polish")
+    print("3. Generate Symbols")
     print("4. Koniec")
 
     user_input = int(input("Give your input: "))
