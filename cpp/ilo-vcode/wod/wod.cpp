1ebc76266c99d874780d8efd52f627b5e0f50b0e
z4v0 <alikdelta@wp.pl>
2021-06-05 14:55:39 +0200

    a

--- cpp/ilo-vcode/wod/wod.cpp
+++ cpp/ilo-vcode/wod/wod.cpp
@@ -13,10 +13,12 @@ using namespace std;
 #define putc_unlocked _fputc_nolock
 #endif
 
-int id[110011];
-short memCount[110011];
+string id[110011];
+int memCount[110011];
 string cityCode[110011];
-int waterUsage[20][110011];
+int waterUsage[110011][20];
+int yearlyWaterUsage[110011];
+pair<float, int> waterPerPerson[110011];
 
 //=======================================
 
@@ -47,15 +49,30 @@ int main() {
     if (fin.is_open()) {
         int i = 0;
         while (getline(fin, line)) {
-            vector<string> info = mysplit(line);
-            for (int j = 0; j < 12; j++) waterUsage[i][j] = stoi(info[i+1]);
-            id[i] = stoi(info[0].substr(0, 5));
-            memCount[i] = stoi(info[0].substr(5, 7));
+            if(i > 0){
+                vector<string> info = mysplit(line);
 
-            cout << id[i] << ' ' << memCount[i] << ' ' << waterUsage[0] << '\n';
+                for (int j = 0; j < 12; j++) {
+                    waterUsage[i][j] = stoi(info[j+1]);
+                    yearlyWaterUsage[i] += waterUsage[i][j];
+                }
+                id[i] = info[0].substr(0, 5);
+                memCount[i] = stoi(info[0].substr(5, 7));
+                cityCode[i] = info[0].substr(7, 10);
+                
+                waterPerPerson[i].first = round(yearlyWaterUsage[i] / memCount[i] * 100.0) / 100.0;
+                waterPerPerson[i].second = i;
+
+                fout << id[i] << ' ' << memCount[i] << ' ' << cityCode[i] << ' ' << waterUsage[i][0] << '\n';
+            }
             i++;
         }
-		fin.close();
+        fin.close();
+    }
+    int n = sizeof(waterPerPerson) / sizeof(waterPerPerson[0]);
+    sort(waterPerPerson, waterPerPerson + n, greater<pair<float, int>>());
+    for (int j = 0; j < 10; j++) {
+        cout << waterPerPerson[j].first << ' ' << id[waterPerPerson[j].second] << '\n';
     }
     return 0;
 }
