efa056d6a7143de76b4a8e9353adde7ef3e0c14d
z4v0 <alikdelta@wp.pl>
2021-09-22 11:12:04 +0200

    yf

--- cpp/ilocamp/dom/dom.cpp
+++ cpp/ilocamp/dom/dom.cpp
@@ -3,31 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-long long k, pointer, n, suma, score;
+long long k, p2, p, n, suma1, suma2;
 long long a[1000111];
 
 int main(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
  
-        cin >> n >> k;
-        pointer = n;
-        for (int i = 1; i <= n; i++){
-            cin >> a[i];
-        }
-        while (suma < k) {
-            suma += a[pointer];
-            pointer--;
-        }
-        score = pointer;
-        pointer++;
-        for(long long i = 1; i <= n; i++){
-            suma += a[i];
-            while (suma - a[pointer] >= k && pointer <= n){
-                suma -= a[pointer];
-                score = max(score, pointer - i);
-                pointer++;
-            }
-        }
-        cout << score;
+    cin >> n >> k;
+    p2 = n;
+    p = 1;
+    for (int i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+    while (suma1 < k) {
+        suma1 += a[p2];
+        p2--;
+    }
+    while (suma2 < k) {
+        suma2 += a[p];
+        p++;
+    }
+    cout << max(n-p, n-p2);
 }
