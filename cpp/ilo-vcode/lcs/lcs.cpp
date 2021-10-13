3d06931a74c940e8d5568f9a379fa7ed382b8cec
z4v0 <alikdelta@wp.pl>
2021-10-13 11:47:22 +0200

    y

---
+++ cpp/ilo-vcode/lcs/lcs.cpp
@@ -0,0 +1,37 @@
+//od tylu do przodu, 
+//snippet
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+#ifdef _WIN32
+#define getc_unlocked _fgetc_nolock
+#define putc_unlocked _fputc_nolock
+#endif
+
+//=======================================
+
+//=======================================
+
+string a, b;
+string D[3000][3000];
+
+int main() {
+    iostream::sync_with_stdio(false);
+    cin >> a >> b;
+
+    int n = a.size();
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+        
+        }
+    }
+    //https://eduinf.waw.pl/inf/alg/001_search/0057.php
+}
