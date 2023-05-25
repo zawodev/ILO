94f0529ab76f391b221e370d16d7db0632f65674
z4v0 <alikstepaniuk@gmail.com>
2023-05-25 23:40:22 +0200

    f

--- java/sem2/so-lab/src/Program5.java
+++ java/sem2/so-lab/src/Program5.java
@@ -1,124 +1,263 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 import java.util.Random;
 
 public class Program5 {
     public static class Processor{
-        public Processor(int maxPower){
+        public Processor(int id, int maxPower){
+            this.id = id;
             this.maxPower = maxPower;
-        }
-        public Processor(){
-            maxPower = 1000;
-        }
-        int currentPowerLevel = 0;
-        int maxPower;
-        int time = 0;
-        public ArrayList<Process> processes = new ArrayList<>();
 
-        int powerLevelZapytanieCount = 0;
+            processes = new ArrayList<>();
+            migrationCount = 0;
+            powerLevelInquiryCount = 0;
+            currentPowerLevel = 0;
+            timePassed = 0;
+            powerLevelSum = 0;
+        }
+        int id;
+        int currentPowerLevel;
+        int maxPower;
+        public ArrayList<Process> processes;
+        public int migrationCount;
+        public int powerLevelInquiryCount;
+        public int powerLevelSum;
+        public int timePassed;
+
 
         public float getPowerLevel() {
-            powerLevelZapytanieCount++;
             return (float)currentPowerLevel / (float)maxPower;
         }
-        public int getPowerLevelZapytanieCount(){
-            return powerLevelZapytanieCount;
+        public float getAveragePowerLevel(){
+            return (float) powerLevelSum / (float)timePassed;
         }
-        public int nextTime(){
+        public void migrate(){
+            migrationCount++;
+        }
+        public void countPowerLevel(){
+            powerLevelInquiryCount++;
+        }
+
+        public String toString(){
+            return "Processor" + id + ", Unfinished Processes: " + processes.toString() + ":\nA) Average Processor Power : " + getAveragePowerLevel() + "\nC) MigrationCount: " + migrationCount + ", PowerLevelInquiryCount: " + powerLevelInquiryCount;
+        }
+        public void nextTime(int val){
+            powerLevelSum += getPowerLevel() * val;
+            timePassed += val;
+
+            if(processes.isEmpty()) return;
+
+            ArrayList<Process> endedProcesses = new ArrayList<>();
             for(Process process : processes){
-                if(process.decrementTime() <= 0){
+                if(process.decrementTime(val)){
                     currentPowerLevel -= process.powerConsumption;
-                    processes.remove(process);
+                    endedProcesses.add(process);
                 }
             }
+            processes.removeAll(endedProcesses);
         }
         public boolean addProcess(Process process){
             currentPowerLevel += process.powerConsumption;
             return processes.add(process);
         }
+        public Process removeRandomProcess(){
+            Random random = new Random();
+            Process process = processes.remove(random.nextInt(processes.size()));
+            currentPowerLevel -= process.powerConsumption;
+            return process;
+        }
     }
     public static class Process{
-        public Process(int powerConsumption, int time){
+        public Process(int powerConsumption, int time, int timeToNext){
             this.powerConsumption = powerConsumption;
             this.time = time;
+            this.timeToNext = timeToNext;
         }
         int powerConsumption;
-        public int getPowerConsumption(){
-            return powerConsumption;
-        }
         int time;
+        int timeToNext;
         public int getTime(){
             return time;
         }
-        public int decrementTime(){
-            return --time;
+        public int getPowerConsumption(){
+            return powerConsumption;
+        }
+        public boolean decrementTime(int val){
+            time -= val;
+            return time <= 0;
+        }
+        public String toString(){
+            return Integer.toString(powerConsumption);
         }
     }
-    public static ArrayList<Processor> generateProcessors(int N, int p){
+    public static ArrayList<Processor> generateProcessors(int N, int maxPower){
         ArrayList<Processor> processors = new ArrayList<>();
         for(int i = 0; i < N; i++){
-            processors.add(new Processor(p));
+            processors.add(new Processor(i + 1, maxPower));
         }
         return processors;
     }
-    public static ArrayList<Process> generateReferenceString(int length, int maxPowerConsumption, int maxTime) {
+    public static ArrayList<Process> generateReferenceString(int length, int minPowerConsumption, int maxPowerConsumption, int minTime, int maxTime, int minTimeToNext, int maxTimeToNext) {
         ArrayList<Process> referenceString = new ArrayList<Process>();
         Random random = new Random();
         for (int i = 0; i < length; i++) {
-            referenceString.add(new Process(random.nextInt(maxPowerConsumption), random.nextInt(maxTime)));
+            referenceString.add(new Process(random.nextInt(minPowerConsumption, maxPowerConsumption), random.nextInt(minTime, maxTime), random.nextInt(minTimeToNext, maxTimeToNext)));
         }
         return referenceString;
     }
-    public static void Strat1(ArrayList<Process> refString, int N, int p, int z){
-        ArrayList<Processor> processors = generateProcessors(N, 1000);
+    public static ArrayList<Process> copyReferenceString(ArrayList<Process> referenceString){
+        ArrayList<Process> referenceString2 = new ArrayList<Process>();
+        for (int i = 0; i < referenceString.size(); i++) {
+            referenceString2.add(new Process(referenceString.get(i).powerConsumption, referenceString.get(i).time, referenceString.get(i).timeToNext));
+        }
+        return referenceString2;
+    }
+    public static void Strategy1(ArrayList<Process> refString, int N, float p, int z, int maxPower){
+        ArrayList<Processor> processors = generateProcessors(N, maxPower);
+        ArrayList<Process> refString2 = copyReferenceString(refString);
+
         Random random = new Random();
-        for(Process process : refString){
-            Processor processor = processors.get(random.nextInt(processors.size()));
+        for(Process process : refString2){
+            Processor processor = processors.get(random.nextInt(0, processors.size()));
             for(int i = 0; i < z; i++){
-                Processor processor2 = processors.get(random.nextInt(processors.size()));
+                Processor processor2 = processors.get(random.nextInt(0, processors.size()));
+                processor2.countPowerLevel();
                 if(processor2.getPowerLevel() <= p){
+                    processor.migrate();
                     processor = processor2;
                     break;
                 }
             }
             processor.addProcess(process);
+
+            for(Processor processor1 : processors){
+                processor1.nextTime(process.timeToNext);
+            }
         }
+        showResults("[Strategy 1]", processors);
     }
-    public static void Strat2(ArrayList<Process> refString, int N, int p){
-        ArrayList<Processor> processors = generateProcessors(N, 1000);
+    public static void Strategy2(ArrayList<Process> refString, int N, float p, int maxPower){
+        ArrayList<Processor> processors = generateProcessors(N, maxPower);
         ArrayList<Processor> processors2 = new ArrayList<>(processors);
+
+        ArrayList<Process> refString2 = copyReferenceString(refString);
+
         Random random = new Random();
-        for(Process process : refString){
-            Processor processor = processors.get(random.nextInt(processors.size()));
+        for(Process process : refString2){
+            Processor processor = processors.get(random.nextInt(0, processors.size()));
+            processor.countPowerLevel();
             if(processor.getPowerLevel() > p){
+                Collections.shuffle(processors2);
                 for(Processor processor2 : processors2){
+                    processor2.countPowerLevel();
                     if(processor2.getPowerLevel() <= p){
+                        processor.migrate();
                         processor = processor2;
                         break;
                     }
-                    else processors2.remove(processor2); //can i do this???
+                    //else processors2.remove(processor2); //can i do this???
                 }
             }
             processor.addProcess(process);
+            //System.out.println(processor.getPowerLevel());
+            //System.out.println(process.powerConsumption);
+
+            for(Processor processor1 : processors){
+
+                processor1.nextTime(process.timeToNext);
+            }
         }
+        showResults("[Strategy 2]", processors);
     }
-    public static void Strat3(ArrayList<Process> refString, int N, int p, int r){
-        ArrayList<Processor> processors = generateProcessors(N, 1000);
+    public static void Strategy3(ArrayList<Process> refString, int N, float p, float r, float takePercent, int maxPower){
+        ArrayList<Processor> processors = generateProcessors(N, maxPower);
+        ArrayList<Processor> processors2 = new ArrayList<>(processors);
+        ArrayList<Process> refString2 = copyReferenceString(refString);
+
         Random random = new Random();
-        for(Process process : refString){
+        for(Process process : refString2){
             Processor processor = processors.get(random.nextInt(processors.size()));
+            processor.countPowerLevel();
+            if(processor.getPowerLevel() > p){
+                Collections.shuffle(processors2);
+                for(Processor processor2 : processors2){
+                    processor2.countPowerLevel();
+                    if(processor2.getPowerLevel() <= p){
+                        processor.migrate();
+                        processor = processor2;
+                        break;
+                    }
+                    //else processors2.remove(processor2); //can i do this???
+                }
+            }
+            processor.addProcess(process);
 
+            for(Processor processor1 : processors){
+                //System.out.println(processor1);
+                processor1.countPowerLevel();
+                if(processor1.getPowerLevel() < r){
+                    Collections.shuffle(processors2);
+                    for(Processor processor2 : processors2){
+                        processor2.countPowerLevel();
+                        if(processor2.getPowerLevel() > p){
+                            processor.migrate();
+                            while(processor2.getPowerLevel() > (1 - takePercent)) {
+                                processor1.addProcess(processor2.removeRandomProcess());
+                            }
+                            break;
+                        }
+                        //else processors2.remove(processor2); //can i do this???
+                    }
+                }
+            }
+            for(Processor processor1 : processors){
+                processor1.nextTime(process.timeToNext);
+            }
         }
+        showResults("[Strategy 3]", processors);
     }
-    public static void main(String[] args) {
-        int p = 0;
-        int r = 0;
-        int z = 0;
-        int N = 0;
+    public static void showResults(String stratName, ArrayList<Processor> processors){
+        System.out.println("\n" + stratName + "\n");
+        float avPowerLevel = 0;
+        float a1 = 0;
+        float a2 = 0;
+        double a3 = 0;
+        for(Processor processor : processors){
+            //System.out.println(processor.toString());
+            avPowerLevel += processor.getAveragePowerLevel();
+            a1 += processor.migrationCount;
+            a2 += processor.powerLevelInquiryCount;
+        }
+        avPowerLevel /= processors.size();
 
-        ArrayList<Process> refString = generateReferenceString(1000, 100, 100);
-        Strat1(refString, N, p, z);
-        Strat2(refString, N, p);
-        Strat3(refString, N, p, r);
+        for(Processor processor : processors){
+            a3 += Math.pow(processor.getAveragePowerLevel() - avPowerLevel, 2);
+        }
+        a3 /= (processors.size() - 1);
+        a3 = Math.sqrt(a3);
+
+        System.out.println("AveragePowerLevel: " + avPowerLevel + ", Odchylenie Standardowe: " + a3 + ", MigrationCount: " + a1 + ", PowerLevelInquiryCount: " + a2);
+    }
+    //public final static int maxTime = 1000;
+    public static void main(String[] args) {
+        float p = .6f; //maksymalny prog po ktorym za duze obciazenie [0,1], gdzie 1 to max
+        float r = .2f; //minimalny próg... od 0 do 1
+        int z = 1; //ilosc wyszukan losowego procka w strat1
+        int N = 100; //ilosc procesorow
+        int maxPower = 100; //maksymalna pojemnosc kazdego procesora
+
+        int minPowerConsumption = 70; //minimalny pobor mocy procesu
+        int maxPowerConsumption = 100; //maksymalny pobor mocy procesu
+        int minTime = 300; //minimalny czas trwania wykonania procesu
+        int maxTime = 400; //maksymalny czas trwania wykonania procesu
+        int minTimeToNext = 1; //minimalny czas pomiedzy procesami
+        int maxTimeToNext = 5; //maksymalny czas pomiedzy procesami
+        float takePercent = .3f; //procent zadan ktore przejmuje proces w strat3
+
+        ArrayList<Process> refString = generateReferenceString(1000, minPowerConsumption, maxPowerConsumption, minTime, maxTime, minTimeToNext, maxTimeToNext);
+        System.out.println(refString);
+        Strategy1(refString, N, p, z, maxPower);
+        Strategy2(refString, N, p, maxPower);
+        Strategy3(refString, N, p, r, takePercent, maxPower);
     }
 }
